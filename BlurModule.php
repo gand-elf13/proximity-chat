0a0d7bee98168ea65cc59925a60ad89ae339e54a
Red-Industrie <146063004+Red-Industrie@users.noreply.github.com>
2023-11-24 12:39:34 +0100

    commentaire du fichier d'altération du msg
    ajout de commentaire à chaque ligne
    & explication du pseudo

--- BlurModule.php
+++ BlurModule.php
@@ -1,20 +1,22 @@
 <?php
-function blur ($message, $percent)
+// code pour alterer un msg en fonction de la distance
+function blur ($message, $percent) # definition de la fonction d'alteration
 {
-	$blurredMessage = "";
+	$blurredMessage = ""; // definition de la variable msg alterer comme etant vide
 	$blurredChars = array ("@", "&", "#", ";", "!", "*", "µ", "£", "$", "€", ",", "'", '"', "`", "~", "^", "à", "é", "ê", "â", "ù");
-	foreach (str_split ($message) as $c)
+	// liste des caracteres utiliser pour alterer le msg
+	foreach (str_split ($message) as $c) // decoupage du msg en une liste de caracteres car php ne le fait pas automatiquement
 	{
-		$randFloat = rand () / getrandmax ();
-		if ($randFloat > $percent)
+		$randFloat = rand () / getrandmax (); // divison du nb par un nombre aleatoire le + grand possible
+		if ($randFloat > $percent) # on cherche a savoir si le resultat de la division est superieur au pourcentage de modification du msg
 		{
-			$blurredMessage = $blurredMessage . $c;
+			$blurredMessage = $blurredMessage . $c; // on renvoi un caractere non-modifie pour remplacer le caractère d'avant
 		}
-		else
+		else // cas ou le resultat de la division est inferieur ou egal au pourcentage d'alteration du msg
 		{
-			$randIndex = rand (0, count ($blurredChars) - 1);
-			$blurredMessage = $blurredMessage . $blurredChars [$randIndex];
+			$randIndex = rand (0, count ($blurredChars) - 1); # on choisit un caractere aleatoire de remplacement parmi la listre au dessus
+			$blurredMessage = $blurredMessage . $blurredChars [$randIndex]; # on renvoi le msg avec le caractere altéré
 		}
 	}
-	return $blurredMessage;
+	return $blurredMessage; # renvoi du msg altéré apres que chaque caractere a verifier si il doit etre alterer ou non
 }
