cd1bac32da830e8879fb6f8d64a6050069a1e042
Leonardo Racic <145390423+leonardo-racic@users.noreply.github.com>
2023-12-01 13:02:06 +0100

    Code correction
    - i continued coding and made a message label tscn to simplify the code

--- MobileVersion/Scripts/ChatScrollContainer.gd
+++ MobileVersion/Scripts/ChatScrollContainer.gd
@@ -2,8 +2,21 @@ class_name ChatScrollContainer
 extends ScrollContainer
 
 
-@onready var username_input := get_tree().current_scene.get_node("%UsernameTextEdit") as TextEdit
-@onready var message_input := get_tree().current_scene.get_node("%MessageTextEdit") as TextEdit
+@onready var parent := get_parent () as VBoxContainer 
+@onready var username_input := parent.get_node ("%UsernameTextEdit") as TextEdit
+@onready var message_input := parent.get_node ("%MessageTextEdit") as TextEdit
 
-func _on_button_pressed() -> void:
-	print(username_input.text)
+@onready var MessageLabelScene := preload("res://message_label.tscn")
+
+
+func add_message(display_text: StringName) -> void:
+	pass
+
+
+func _on_button_pressed () -> void:
+	var username: StringName = username_input.text
+	var message: StringName = message_input.text
+	if username and message:
+		var display_text: StringName = &"[b]%s : [/b]%s" % [username, message]
+		
+	print (username_input.text)
