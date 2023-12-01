48eef4daa5070d5c741f1f6c4919331dd0693149
Leonardo Racic <145390423+leonardo-racic@users.noreply.github.com>
2023-12-01 22:44:51 +0100

    Update Mobile Version
    - I changed some node types and continued coding. Now we need a system to get the user's location

--- MobileVersion/Scripts/ChatScrollContainer.gd
+++ MobileVersion/Scripts/ChatScrollContainer.gd
@@ -3,14 +3,19 @@ extends ScrollContainer
 
 
 @onready var parent := get_parent () as VBoxContainer 
-@onready var username_input := parent.get_node ("%UsernameTextEdit") as TextEdit
-@onready var message_input := parent.get_node ("%MessageTextEdit") as TextEdit
+@onready var username_input := parent.get_node ("%UsernameLineEdit") as LineEdit
+@onready var message_input := parent.get_node ("%MessageLineEdit") as LineEdit
+@onready var message_container := get_node ("MessageVBoxContainer") as VBoxContainer
 
-@onready var MessageLabelScene := preload("res://message_label.tscn")
+@onready var MessageLabelScene: PackedScene = preload ("res://message_label.tscn")
 
 
-func add_message(display_text: StringName) -> void:
-	pass
+func add_message (display_text: StringName) -> void:
+	var new_message := MessageLabelScene.instantiate ()
+	message_container.call_deferred ("add_child", new_message)
+	await message_container.child_entered_tree
+	message_container.call_deferred ("move_child", new_message, 0)
+	new_message.set_text (display_text)
 
 
 func _on_button_pressed () -> void:
@@ -18,5 +23,6 @@ func _on_button_pressed () -> void:
 	var message: StringName = message_input.text
 	if username and message:
 		var display_text: StringName = &"[b]%s : [/b]%s" % [username, message]
-		
-	print (username_input.text)
+		add_message (display_text)
+	message_input.set_text (&"")
+	
