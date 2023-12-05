a80e7f9ac641952b6fad389d78a9a9df0683fad9
Leonardo Racic <145390423+leonardo-racic@users.noreply.github.com>
2023-12-05 22:56:50 +0100

    MobileVersion update
    - I made it easier to read and use for mobile users

--- MobileVersion/script.js
+++ MobileVersion/script.js
@@ -5,18 +5,32 @@ sendButton.addEventListener("click", function(event) {
 });
 
 
-function createMessage() {
-	const p = document.createElement("p");
+function addMessage(usernameText, messageText) {
 	const spanUsername = document.createElement("span");
 	spanUsername.classList.add("username");
-	const usernameTextNode = document.createTextNode("Username-NEW: ");	
+	const usernameTextNode = document.createTextNode(usernameText + ": ");	
 	spanUsername.appendChild(usernameTextNode);
 	
 	const spanMessage = document.createElement("span");
 	spanMessage.classList.add("message");
+	const messageTextNode = document.createTextNode(messageText);
+	spanMessage.appendChild(messageTextNode);
 	
-	
+	const p = document.createElement("p");
 	p.appendChild(spanUsername);
+	p.appendChild(spanMessage);
+
 	const chatbox = document.querySelector("#chatbox");
 	chatbox.appendChild(p);
+}
+
+
+function createMessage() {
+	const usernameInput = document.querySelector("#username-input");
+	const messageInput = document.querySelector("#message-input");
+	const username = usernameInput.value;
+	const message = messageInput.value;
+	if ((username.trim().length !== 0) && (message.trim().length !== 0)) {
+		addMessage(username, message)
+	}
 }
