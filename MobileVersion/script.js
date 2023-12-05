afa9975b33286ade460b90deda5c92d666981bbb
Leonardo Racic <145390423+leonardo-racic@users.noreply.github.com>
2023-12-05 09:51:59 +0100

    Continued
    - I haven't finished, but it is starting to work. Now we gotta see how we're going to have access to the php database....

--- MobileVersion/script.js
+++ MobileVersion/script.js
@@ -1,5 +1,22 @@
-var sendButton = document.getElementById("send-button");
-sendButton.addEventListener("onclick", onSentButtonClicked());
-function onSentButtonClicked() {
-	sendButton.value = "Sent!";
+const sendButton = document.querySelector("#send-button");
+sendButton.addEventListener("click", function(event) {
+	event.preventDefault();
+	createMessage();
+});
+
+
+function createMessage() {
+	const p = document.createElement("p");
+	const spanUsername = document.createElement("span");
+	spanUsername.classList.add("username");
+	const usernameTextNode = document.createTextNode("Username-NEW: ");	
+	spanUsername.appendChild(usernameTextNode);
+	
+	const spanMessage = document.createElement("span");
+	spanMessage.classList.add("message");
+	
+	
+	p.appendChild(spanUsername);
+	const chatbox = document.querySelector("#chatbox");
+	chatbox.appendChild(p);
 }
